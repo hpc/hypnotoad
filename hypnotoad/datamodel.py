93057fab9a2d4ebb337233846d60754443c4dec2
Jon Bringhurst <jonb@lanl.gov>
2011-12-13 13:49:49 -0700

    Added basic plugin classes.

---
+++ hypnotoad/datamodel.py
@@ -0,0 +1,16 @@
+class DataModelPlugin(object):
+    def setup(self):
+        """Called before the plugin is asked to do anything."""
+        raise NotImplementedError
+ 
+    def teardown(self):
+        """Called to allow the plugin to free anything."""
+        raise NotImplementedError
+ 
+    def user_info(self):
+        """Look up user information in this data model."""
+        raise NotImplementedError
+
+    def priority_info(self):
+        """Look up priority information in this data model."""
+        raise NotImplementedError
---
+++ hypnotoad/scheduler.py
@@ -0,0 +1,16 @@
+class SchedulerPlugin(object):
+    def setup(self):
+        """Called before the plugin is asked to do anything."""
+        raise NotImplementedError
+ 
+    def teardown(self):
+        """Called to allow the plugin to free anything."""
+        raise NotImplementedError
+
+    def user_output(self):
+        """Return user information formatted for this scheduler."""
+        raise NotImplementedError
+
+    def priority_output(self):
+        """Return priority information formatted for this scheduler."""
+        raise NotImplementedError
