1ffe7f02d8cdce20d32cf9f380a08ba84214bab7
Jonathan O. Bringhurst <jonb@rry.lanl.gov>
2012-01-04 11:09:14 -0700

    Started generating moab config.

--- plugins/schedulers/moab_plugin.py
+++ plugins/schedulers/moab_plugin.py
@@ -5,8 +5,10 @@
 from hypnotoad import plugin
 import ldap
 import logging
+import pprint
 
 LOG = logging.getLogger('root')
+PP = pprint.PrettyPrinter(indent=4)
 
 class moab_plugin(plugin.scheduler_plugin):
     def setup(self, config, model_version):
@@ -27,8 +29,20 @@ class moab_plugin(plugin.scheduler_plugin):
         if self.plugin_enabled:
             LOG.debug("Got to moab plugin teardown")
 
-    def append_model(self, model):
+    def append_model(self, models):
         """Handled a model appended to this output."""
 
         if self.plugin_enabled:
             LOG.debug("Got to moab plugin append_model.")
+
+            for plug_model in models:
+                for m in plug_model:
+                    if 'group_entry' in m.keys():
+                        group = m['group_entry']
+
+                        name = group['short_name_string']
+                        fstarget = group['priority_fairshare_float']
+
+                        LOG.debug('Config entry: GROUPCFG[%s] FSTARGET=%s' % (name, fstarget))
+
+#            PP.pprint(model)
