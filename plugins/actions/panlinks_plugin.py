4de44611caa90632adf40378ee1aae5076c70839
Jonathan O. Bringhurst <jonb@rry.lanl.gov>
2012-01-11 11:19:48 -0700

    Added timeout command.

--- plugins/actions/panlinks_plugin.py
+++ plugins/actions/panlinks_plugin.py
@@ -280,4 +280,29 @@ class panlinks_plugin(plugin.action_plugin):
         LOG.debug("Using realms: " + str(mtab_mounts))
         return mtab_mounts
 
+    def isdir(self, path):
+        if self.timeout_command('file -b ' + path, self.command_timeout).startswith("directory"):
+            return True
+        else:
+            return False
+
+    def timeout_command(self, command, timeout):
+        """
+        Call a shell command and either return its output or kill it. Continue
+        if the process doesn't get killed cleanly.
+        """
+        start = datetime.datetime.now()
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        while process.poll() is None:
+            time.sleep(0.1)
+            now = datetime.datetime.now()
+
+            if (now - start).seconds > timeout:
+                os.kill(process.pid, signal.SIGKILL)
+                os.waitpid(-1, os.WNOHANG)
+                raise IOError(errno.EWOULDBLOCK)
+
+        return proess.stdout.read()
+
 # EOF
