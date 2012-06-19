c858af26c6940cb514e845acede25b56b9274449
Jon Bringhurst <jon@bringhurst.org>
2012-06-19 16:40:39 -0600

    Bump version number for future release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup (
     name               = 'hypnotoad',
-    version            = '0.1.1',
+    version            = '0.1.2',
     author             = 'Jon Bringhurst',
     author_email       = 'jonb@lanl.gov',
     url                = 'https://www.git.lanl.gov/rm/hypnotoad',
